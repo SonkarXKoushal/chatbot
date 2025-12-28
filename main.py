ee13982c8b52b6a72c66d6c0a1a9d43545239117
SonkarXKoushal <sonkarp463@gmail.com>
2025-12-28 18:10:56 +0530

    Refactor main.py to correct imports and indentation
    Fix indentation and duplicate import statements.

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+aimport asyncio
+from nexichat.userbot.userbot import Userbot
+
+if __name__ == "__main__":
+    userbot = Userbot()
+    asyncio.get_event_loop().run_until_complete(userbot.start())
 from nexichat.userbot.userbot import Userbot
 
 if __name__ == "__main__":
