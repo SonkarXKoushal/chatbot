119725e7f2e62bebb73958cc4ac6a439d9331b58
SonkarXKoushal <sonkarp463@gmail.com>
2025-12-28 18:39:49 +0530

    Refactor main.py to use run function

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-import asyncio
-import signal
-from pyrogram import idle
-from nexichat.userbot.userbot import Userbot
-
-userbot = Userbot()
-
-async def main():
-    await userbot.start()
-    await idle()
-    await userbot.stop()
+from nexichat.bot import run
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    run()
